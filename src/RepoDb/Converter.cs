90e275d4ad447640ff747f591a6b23b27a5df40a
Bert Huijben <bert@qqmail.nl>
2025-06-19 19:48:21 +0200

    Fix license expression

--- src/RepoDb/Converter.cs
+++ src/RepoDb/Converter.cs
@@ -178,7 +178,7 @@ public static class Converter
         }
     }
 
-
+#if !NET
     static object? EnumTryParse<TEnum>(string value)
         where TEnum : struct, Enum
     {
@@ -186,6 +186,7 @@ public static class Converter
             return result;
         return null;
     }
+#endif
 
     #endregion
 }
