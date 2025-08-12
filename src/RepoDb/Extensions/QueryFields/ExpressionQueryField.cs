abae697d72d4d7b9caf7020a45a0ae4055719345
Bert Huijben <bert@qqmail.nl>
2025-08-12 16:14:28 +0200

    Fix code style

--- src/RepoDb/Extensions/QueryFields/ExpressionQueryField.cs
+++ src/RepoDb/Extensions/QueryFields/ExpressionQueryField.cs
@@ -2,38 +2,37 @@ using RepoDb;
 using RepoDb.Enumerations;
 using RepoDb.Interfaces;
 
-namespace RepoDb.Extensions.QueryFields
+namespace RepoDb.Extensions.QueryFields;
+
+/// <summary>
+/// Abstract base for query fields representing expressions (e.g., field-to-field, arithmetic, etc.).
+/// </summary>
+public abstract class ExpressionQueryField : QueryField
 {
-    /// <summary>
-    /// Abstract base for query fields representing expressions (e.g., field-to-field, arithmetic, etc.).
-    /// </summary>
-    public abstract class ExpressionQueryField : QueryField
+    public IEnumerable<Field> Fields { get; }
+
+    protected ExpressionQueryField(IEnumerable<string> fields, Operation operation)
+        : this(fields.Select(x => new Field(x)), operation)
     {
-        public IEnumerable<Field> Fields { get; }
-
-        protected ExpressionQueryField(IEnumerable<string> fields, Operation operation)
-            : this(fields.Select(x => new Field(x)), operation)
-        {
-        }
-
-        protected ExpressionQueryField(IEnumerable<Field> fields, Operation operation)
-                    : base(fields.First(), operation, null, null)
-        {
-            Fields = fields;
-        }
-
-        public override string GetString(int index, IDbSetting dbSetting)
-        {
-            return GetString(dbSetting);
-        }
-
-        protected override string GetString(int index, string? functionFormat, IDbSetting dbSetting)
-        {
-            return GetString(dbSetting);
-        }
-
-        public abstract override string GetString(IDbSetting dbSetting);
-
-        protected internal override bool NoParametersNeeded => true;
     }
+
+    protected ExpressionQueryField(IEnumerable<Field> fields, Operation operation)
+                : base(fields.First(), operation, null, null)
+    {
+        Fields = fields;
+    }
+
+    public override string GetString(int index, IDbSetting dbSetting)
+    {
+        return GetString(dbSetting);
+    }
+
+    protected override string GetString(int index, string? functionFormat, IDbSetting dbSetting)
+    {
+        return GetString(dbSetting);
+    }
+
+    public abstract override string GetString(IDbSetting dbSetting);
+
+    protected internal override bool NoParametersNeeded => true;
 }
--- src/RepoDb/Extensions/QueryFields/FieldComparisonQueryField.cs
+++ src/RepoDb/Extensions/QueryFields/FieldComparisonQueryField.cs
@@ -1,26 +1,25 @@
 using RepoDb.Enumerations;
 using RepoDb.Interfaces;
 
-namespace RepoDb.Extensions.QueryFields
-{
-    /// <summary>
-    /// Represents a comparison between two fields (columns) in a query expression.
-    /// </summary>
-    public class FieldComparisonQueryField : ExpressionQueryField
-    {
-        public Field Left { get; }
-        public Field Right { get; }
-        public FieldComparisonQueryField(Field left, Operation operation, Field right)
-            : base([left, right], operation)
-        {
-            Left = left;
-            Right = right;
-        }
+namespace RepoDb.Extensions.QueryFields;
 
-        public override string GetString(IDbSetting dbSetting)
-        {
-            // Example: [Left] = [Right]
-            return $"{Left.FieldName.AsField(dbSetting)} {Operation.GetText()} {Right.FieldName.AsField(dbSetting)}";
-        }
+/// <summary>
+/// Represents a comparison between two fields (columns) in a query expression.
+/// </summary>
+public sealed class FieldComparisonQueryField : ExpressionQueryField
+{
+    public Field Left { get; }
+    public Field Right { get; }
+    public FieldComparisonQueryField(Field left, Operation operation, Field right)
+        : base([left, right], operation)
+    {
+        Left = left;
+        Right = right;
+    }
+
+    public override string GetString(IDbSetting dbSetting)
+    {
+        // Example: [Left] = [Right]
+        return $"{Left.FieldName.AsField(dbSetting)} {Operation.GetText()} {Right.FieldName.AsField(dbSetting)}";
     }
 }
